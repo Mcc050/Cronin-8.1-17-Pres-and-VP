481aa1f70fc289d18e0b552e28c207f5f98db77e
MCC050 <michaelconlycronin@gmail.com>
2018-01-11 16:32:55 -0600

    Initial commit

---
+++ Cronin 8.1-17 Pres and VP/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        Dim VP() As String = IO.File.ReadAllLines("VPres.txt")
+        Dim Prez() As String = IO.File.ReadAllLines("USPres.txt")
+
+        Dim union() As String = Prez.Union(VP).ToArray
+        Dim intersect() As String = Prez.Intersect(VP).ToArray
+        Dim final() As String = union.Except(intersect).ToArray
+
+
+
+        IO.File.WriteAllLines("Output.txt", final)
+        MessageBox.Show(final.Count & " presidents or vice presidents (but not both)", "File Created")
+
+    End Sub
+End Class
---
+++ Cronin 8.1-17 Pres and VP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Cronin 8.1-17 Pres and VP")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Cronin 8.1-17 Pres and VP")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("38e27f53-4699-47d6-835b-fa62ebe24194")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
